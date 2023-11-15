1c2a3845bbac203cab0f615ef994dc4db8cfdef1
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-15 11:48:01 +0100

    Conversão de moeda

---
+++ aula1/app.js
@@ -0,0 +1,5 @@
+
+var valorAConverter=prompt('Olá, Tudo Bem?\nIntroduza o valor em kz que deseja converter');
+var baseDeConversão=834;
+var valorEmDolar=(valorAConverter/baseDeConversão);
+alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);

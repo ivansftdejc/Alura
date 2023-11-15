3e6c5877843fc4c84d6d95877f85148a611283f2
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-15 11:56:41 +0100

    Implementação cíclica

--- aula1/app.js
+++ aula1/app.js
@@ -1,5 +1,8 @@
-
-var valorAConverter=prompt('Olá, Tudo Bem?\nIntroduza o valor em kz que deseja converter');
-var baseDeConversão=834;
-var valorEmDolar=(valorAConverter/baseDeConversão);
-alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);
+do {
+    var valorAConverter = prompt('Olá, Tudo Bem?\nIntroduza o valor em kz que deseja converter.');
+    var baseDeConversão = 834;
+    var valorEmDolar = (valorAConverter / baseDeConversão);
+    alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);
+    var resp = prompt('Deseja continuar?\nSim ou Não?');
+}
+while (resp == 'Sim')

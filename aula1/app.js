104fe45f491319a8ea4483504cde4b1530638d48
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-16 16:27:43 +0100

    criação de um coversor personalizado

--- aula1/app.js
+++ aula1/app.js
@@ -1,8 +1,14 @@
-do {
-    var valorAConverter = prompt('Olá, Tudo Bem?\nIntroduza o valor em kz que deseja converter.');
-    var baseDeConversão = 834;
-    var valorEmDolar = (valorAConverter / baseDeConversão).toFixed(2);
-    alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);
-    var resp = prompt('Deseja continuar?\nSim ou Não?');
+var op = prompt('Olá seja bem vindo ao nossso conversor!\nO que deseja fazer?\n   1.Coverter moeda;\n   2.Converter distâcias;\n   3.Converter temperatura.\n');
+switch (op) {
+    case '1':
+        do {
+            var valorAConverter = prompt('Introduza o valor em kz que deseja converter.');
+            var baseDeConversão = 834;
+            var valorEmDolar = (valorAConverter / baseDeConversão).toFixed(2);
+            alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);
+            var resp = prompt('Continuar?\nSim ou Não?');
+        }
+        while (resp == 'Sim')
+        
+
 }
-while (resp == 'Sim')

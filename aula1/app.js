9d5f802b10c2461794f2e8e54bed3db348dc033d
ivansftdejc <111021110+ivansftdejc@users.noreply.github.com>
2023-11-16 13:55:14 +0100

    add limitador de casas decimais e modificando a chamada do código js

--- aula1/app.js
+++ aula1/app.js
@@ -1,7 +1,7 @@
 do {
     var valorAConverter = prompt('Olá, Tudo Bem?\nIntroduza o valor em kz que deseja converter.');
     var baseDeConversão = 834;
-    var valorEmDolar = (valorAConverter / baseDeConversão);
+    var valorEmDolar = (valorAConverter / baseDeConversão).toFixed(2);
     alert(`${valorAConverter}kz corresponde a $${valorEmDolar}USD`);
     var resp = prompt('Deseja continuar?\nSim ou Não?');
 }
